f39fca9e1a3f8e86d8a6c941a5de7438ddd47ce7
Shystra <jlsp.2019@hotmail.com>
2023-05-04 09:28:40 -0300

    configuring pasts

---
+++ project/src/Components/useCallback/index.jsx
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './style.css';
+import { useState } from 'react';
+
+export const Reverse = () => {
+    
+
+
+
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
+               
+
+
+
+
+                
+
+            </header>
+        </div>
+    );
+}
