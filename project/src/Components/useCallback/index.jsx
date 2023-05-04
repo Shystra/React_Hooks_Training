49562461bff8ac735d22f8087b8b42405ada321d
Shystra <jlsp.2019@hotmail.com>
2023-05-04 12:39:18 -0300

    configuring incrementButton

--- project/src/Components/useCallback/index.jsx
+++ project/src/Components/useCallback/index.jsx
@@ -2,7 +2,21 @@ import logo from './logo.svg';
 import './style.css';
 import { useState } from 'react';
 
-export const Reverse = () => {
+
+const Button = ({ incrementButton}) => {
+    return <button onClick={() => incrementButton(10)}>+</button>
+}
+
+
+
+
+export const CallBack = () => {
+    const[counter, setCounter] = useState(0)
+
+
+    const incrementCounter = (num) => {
+        setCounter(counter + num)
+    }
     
 
 
@@ -11,11 +25,11 @@ export const Reverse = () => {
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
+                <img src={logo} className="App-logo" alt="logo" />
                
 
-
-
+                <h1>Counter: {counter}</h1>
+                <Button incrementButton={incrementCounter}/>
 
                 
 
--- project/src/index.js
+++ project/src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { Contador } from './Components/useEffect';
+import { Reverse } from './Components/useState';
+import { CallBack } from './Components/useCallback';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Contador/>
+    <Reverse />
+    <CallBack/>
   </React.StrictMode>
 );
 
